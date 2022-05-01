e578d30259b014e8d745421154ea4d09b2eda932
Mohamed Abd-Elhamed Ahmed <73020992+Mohamedabdelhamed382@users.noreply.github.com>
2022-05-01 03:04:33 +0200

    Save User Locally Using User Defaults

---
+++ Dardesh/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Dardesh
+//
+//  Created by MOHAMED ABD ELHAMED AHMED on 01/05/2022.
+//
+
+import Foundation
+
+public let kCurrentUser = "currentUser"
+public let userDefaults = UserDefaults.standard
--- Dardesh/Model/User.swift
+++ Dardesh/Model/User.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseFirestoreSwift
+import RealmSwift
 
 struct User: Codable {
     var id: String?
@@ -15,4 +16,18 @@ struct User: Codable {
     var pushId:String?
     var avatarLink:String?
     var status: String?
+}
+
+//MARK: - save user in userDefulats
+func saveUserLocally(_ user: User) {
+    let encoder  = JSONEncoder()
+    
+    do{
+        
+        let data = try encoder.encode(user)
+        userDefaults.set(data, forKey: kCurrentUser)
+        
+    } catch( let error) {
+        print(error.localizedDescription)
+    }
 }
